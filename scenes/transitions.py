6ff2cbeae3e48c20068f80d8cd3197ddf2e2fad9
Gautier Miquet <xisabla.dev@gmail.com>
2020-02-02 12:13:56 +0100

    Preparing transitions

--- scenes/transitions.py
+++ scenes/transitions.py
@@ -13,3 +13,15 @@ class Transition1(TransitionScene):
     def draw(self, win):
         TransitionScene.draw(self, win)
         win.blit(self.title, self.title.get_rect(center=(self.width / 2, self.height / 2)))
+
+
+class TransitionWin(TransitionScene):
+    def __init__(self):
+        TransitionScene.__init__(self, 'trans_win', 'menu_background.png')
+
+        self.font_title = pygame.font.SysFont('comicsansms', 80)
+        self.title = self.font_title.render('Continue !', True, (255, 255, 255))
+
+    def draw(self, win):
+        TransitionScene.draw(self, win)
+        win.blit(self.title, self.title.get_rect(center=(self.width / 2, self.height / 2)))
